a3cc67bbd1986c1a118743ec46837104c02e0083
Joaquin Puga <joaquin.puga@usm.cl>
2022-07-11 18:28:20 -0400

    creacion nuevo archivo para la pagina

---
+++ productos.php
@@ -0,0 +1,27 @@
+<?php
+include("connection.php");
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="main.css" rel="stylesheet" type="text/css">
+    <title>Floreria Anylu</title>
+</head>
+<body>
+<header>
+        <div id="header">
+            <nav>
+                <ul>
+                    <li><a href="index.html">Inicio</a></li>
+                    <li><a href="#catalogo">Catalogo</a></li>
+                    <li><a href="#contact">Contacto</a></li>
+                    <li><a href="#about">Acerca de</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</body>
