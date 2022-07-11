25ef406cead74d47292d7dfb8875ea033a3eb030
devLucasSsS <107324428+devLucasSsS@users.noreply.github.com>
2022-07-11 16:24:25 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+include("conn.php");
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="main.css" rel="stylesheet" type="text/css">
+    <title>Floreria Anylu</title>
+</head>
+<body>
+    <header>
+        <div id="header">
+            <div id="logo">
+                <img class="logo" src="src/logo.png" alt="Logo">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="index.html">Inicio</a></li>
+                    <li><a href="#catalogo">Catalogo</a></li>
+                    <li><a href="#contact">Contacto</a></li>
+                    <li><a href="#about">Acerca de</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <section id="cuerpo">
+        <a>asd</a>
+
+    </section>
+
+    <footer>
+        PROGRAMACION ORIENTADA A LA WEB 2022
+    </footer>
+</body>
+</html>
