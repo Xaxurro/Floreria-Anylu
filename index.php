c56da290591e6dd2ff90717e6adda6413c2b5747
Joaquin Puga <joaquin.puga@usm.cl>
2022-07-12 04:33:52 -0400

    updates

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
-<?php include("./view/header.php") ?>
-    <section id="cuerpo">
-        <a></a>
-        <img src="./src/daniel-lopez.png" alt="peruano1">
+<?php include("./view/header.php"); ?>
+<section id="cuerpo">
+    <a></a>
+    <img src="./src/daniel-lopez.png" alt="peruano1">
+    
+</section>
+<?php include("./view/footer.php"); ?>
 
-    </section>
-
-</body>
-<footer>
-    PROGRAMACION ORIENTADA A LA WEB 2022
-</footer>
-</html>
