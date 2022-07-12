2d22786c807ebf76823f0a6a935335f98e038d71
Xaxurro <78761239+Xaxurro@users.noreply.github.com>
2022-07-12 03:45:46 -0400

    Update connection.php

--- controller/connection.php
+++ controller/connection.php
@@ -7,7 +7,7 @@ $db="floreria";
 $con = new mysqli($dir,$user,$password,$db);
 
 if($con->connect_error == false){
-    echo "ERROR EN LA CONEXIÓN A LA BASE DE DATOS";
+    echo "ERROR EN LA CONEXIÓN A LA BASE DE DATOS<br>".$con->connect_error;
 }
 
 ?>
