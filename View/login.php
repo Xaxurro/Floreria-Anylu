b62938712d702a6c7cd0635a38bd50c2e78fc269
Xaxurro <78761239+Xaxurro@users.noreply.github.com>
2022-07-14 20:27:05 -0400

    Minor Update

--- View/login.php
+++ View/login.php
@@ -18,17 +18,22 @@
         $con->close();
     }
 ?>
-    <center>
-        <form action="login.php" method="POST">
-            <label for="id">ID:</label><br>
-            <input type="text" name="id" maxlength="255"><br><br>
-            
-            <label for="password">Contraseña:</label><br>
-            <input type="password" name="password" maxlength="24"><br><br>
-            
-            <input type="submit" value="Login"><br>
-        </form>
-    </center>
+    <fieldset>
+        <legend><b><i>Inicie Sesion:</i></b></legend>
+        <center>
+            <br>
+            <form action="login.php" method="POST">
+                <label for="id">ID:</label><br>
+                <input type="text" name="id" maxlength="255"><br><br>
+                
+                <label for="password">Contraseña:</label><br>
+                <input type="password" name="password" maxlength="24"><br><br>
+                
+                <input type="submit" value="Login"><br>
+            </form>
+            <br>
+        </center>
+    </fieldset>
 <?php
 include("./Templates/footer.php"); 
 ?>
