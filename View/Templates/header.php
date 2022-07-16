1b1661526360ba9864407b7e2024471fba4ae9cd
devLucasSsS <107324428+devLucasSsS@users.noreply.github.com>
2022-07-16 03:52:57 -0400

    updates

--- View/Templates/header.php
+++ View/Templates/header.php
@@ -15,6 +15,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../css/main.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <title>Floreria Anylu</title>
 </head>
 <body>
@@ -26,7 +28,7 @@
             </div>
             <nav>
                 <ul>
-                    <li><a href="index.php"id="colorHeader">Inicio</a></li>
+                    <li><a href="index.php"id="colorHeader" >Inicio</a></li>
                     <li><a href="./catalogo.php" id="colorHeader">Catalogo</a></li>
                     <li><a href="./mail.php" id="colorHeader">Agenda tu visita!</a></li>
                     <li><a href="./login.php" id="colorHeader"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/></svg></a></li>
